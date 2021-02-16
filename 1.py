80d4a414008ccddfe8602b9ba7c6ed5f6a181c91
ShonNuo <kloyn2031@yandex.ru>
2021-02-16 22:41:09 +0300

    Слои карт

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ import os
 import requests
 
 # Пусть наше приложение предполагает запуск в командной строке:
-# python search_req.py Москва, ул. Ак. Королева, 12
+# python 1.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
 # http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Москва, ул. Ак. Королева, 12&format=json
 
@@ -73,18 +73,21 @@ def get_ll_span(address):
 
 def show_map(toponym_to_find, spn, map_type="map", add_params=None):
     map_file = "map.png"
+    map_types = ['map', 'sat', 'sat,skl']
+    map_type = map_type
     running = True
     shift = [0, 0]
+    K_pressed = True
     while running:
         ll = get_coordinates(toponym_to_find)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
+                if event.key == pygame.K_PAGEUP:
                     spn[0] += 0.001
                     spn[1] += 0.001
-                elif event.key == pygame.K_s:
+                elif event.key == pygame.K_PAGEDOWN:
                     spn[0] = max(0.0001, spn[0] - 0.001)
                     spn[1] = max(0.0001, spn[1] - 0.001)
                 elif event.key == pygame.K_LEFT:
@@ -95,29 +98,34 @@ def show_map(toponym_to_find, spn, map_type="map", add_params=None):
                     shift[1] += 0.0001
                 elif event.key == pygame.K_DOWN:
                     shift[1] -= 0.0001
-        ll_spn = f"ll={','.join([str(ll[0] + shift[0]), str(ll[1] + shift[1])])}&spn={spn[0]},{spn[1]}"
-        if ll_spn:
-            map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
-        else:
-            map_request = f"http://static-maps.yandex.ru/1.x/?l={map_type}"
+                elif event.key == pygame.K_SPACE:
+                    map_type = map_types[(map_types.index(map_type) + 1) % 3]
+                K_pressed = True
+        if K_pressed:
+            ll_spn = f"ll={','.join([str(ll[0] + shift[0]), str(ll[1] + shift[1])])}&spn={spn[0]},{spn[1]}"
+            if ll_spn:
+                map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
+            else:
+                map_request = f"http://static-maps.yandex.ru/1.x/?l={map_type}"
 
-        if add_params:
-            map_request += "&" + add_params
-        response = requests.get(map_request)
+            if add_params:
+                map_request += "&" + add_params
+            response = requests.get(map_request)
 
-        if not response:
-            pass
+            if not response:
+                pass
 
-        try:
-            with open(map_file, "wb") as file:
-                file.write(response.content)
-        except IOError as ex:
-            print("Ошибка записи временного файла:", ex)
-            sys.exit(2)
+            try:
+                with open(map_file, "wb") as file:
+                    file.write(response.content)
+            except IOError as ex:
+                print("Ошибка записи временного файла:", ex)
+                sys.exit(2)
 
-        screen = pygame.display.set_mode((600, 450))
-        screen.blit(pygame.image.load(map_file), (0, 0))
-        pygame.display.flip()
+            K_pressed = False
+            screen = pygame.display.set_mode((600, 450))
+            screen.blit(pygame.image.load(map_file), (0, 0))
+            pygame.display.flip()
 
     pygame.quit()
     os.remove(map_file)
